15ad89b406eb8624a4fd4a27f9ad00eb089c1c54
apermar1301 <apermar1301@g.educaand.es>
2026-01-23 13:36:38 +0100

    23/01/26: Actividades 4-11 hechas.

--- ACT2_FUNCIONES_TRANSFORMACION/transformacion.sql
+++ ACT2_FUNCIONES_TRANSFORMACION/transformacion.sql
@@ -23,3 +23,31 @@ SELECT
     concat (direccion, ', ', ciudad, ', ', cp, ', ', pais) AS direccion
 FROM
     oficinas;
+
+-- ACT5: Abreviatura de ciudad
+SELECT
+    nombre,
+    ciudad,
+    UPPER(LEFT (ciudad, 3)) AS ciudadAbr
+FROM
+    clientes;
+
+-- ACT6: Logitud del nombre de los productos
+select nombre, length(nombre) as nombreLongitud from clientes;
+
+-- ACT7: Precio con IVA
+Select nombre, round(precio*1.21,2) as 'precio+IVA' from productos;
+
+-- ACT8: Precio sin decimales
+Select nombre, truncate(precio,0) as precioTruncado from productos;
+
+-- ACT9: Resto del stock
+select nombre, MOD(precio, 10) as 'RESTO: stock/10' from productos; 
+
+-- ACT10: Mees de los pedidos
+SET lc_time_names = 'es_ES'; 
+select id, monthname(fecha) as mes from pedidos;
+
+-- ACT11: Fecha actual en los pedidos
+Select nombre, fecha as fechaPedido, curdate() fechaActual from pedidos;
+
