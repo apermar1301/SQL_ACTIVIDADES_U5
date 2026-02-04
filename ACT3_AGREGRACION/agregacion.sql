7d7a500db7e4d7e41118b26576bff5310d6ab198
apermar1301 <apermar1301@g.educaand.es>
2026-02-04 22:12:49 +0100

    04/02/2026: Corrección ortográfica del archivo de agregacion.sql. Ejercicios 12-14 hechos.

--- ACT3_AGREGRACION/agregacion.sql
+++ ACT3_AGREGRACION/agregacion.sql
@@ -73,10 +73,48 @@ GROUP BY
 -- ACT10: Pagos por cliente
 SELECT
     idCliente,
-    COUNT(cantidad) as cantidad
+    COUNT(cantidad) AS cantidad
 FROM
     pagos
 GROUP BY
     idCliente;
 
 -- ACT11: Estados con alto volumen
+SELECT
+    ID,
+    estado,
+    COUNT(*)
+FROM
+    pedidos
+GROUP BY
+    estado
+HAVING
+    COUNT(*) > 10;
+
+-- ACT12: Clientes con grandes pagos acumulados
+SELECT
+    idCliente,
+    SUM(cantidad)
+FROM
+    pagos
+GROUP BY
+    idCliente
+HAVING
+    SUM(cantidad) > 500;
+
+-- ACT13: Valor total del stock por gama
+SELECT
+    idGama,
+    stock * precio as ValorTotal
+from
+    productos
+GROUP by idGama;
+
+-- ACT14: Pedido más reciente por cliente
+Select
+    idCliente,
+    MAX(fecha) AS fecha_ultimo_pedido
+from
+    pedidos
+group by
+    idCliente
