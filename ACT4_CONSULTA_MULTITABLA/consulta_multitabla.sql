9ab5e6709a563015b20421e438497a1ccbb709f4
apermar1301 <apermar1301@g.educaand.es>
2026-02-12 10:21:29 +0100

    12/02/2026: Ejercicio 2-4 hechos

--- ACT4_CONSULTA_MULTITABLA/consulta_multitabla.sql
+++ ACT4_CONSULTA_MULTITABLA/consulta_multitabla.sql
@@ -1,12 +1,33 @@
-
-
--- Pedidos con datos del cliente
-select
+-- ACT1: Pedidos con datos del cliente
+SELECT
     p.id,
     p.fecha,
     p.estado,
-    c.nombre as cliente
+    c.nombre AS cliente
+FROM
+    pedidos AS p
+    INNER JOIN clientes AS c ON p.idCliente = c.id;
+
+-- ACT2: Clientes que han realizado pedidos
+SELECT DISTINCT
+    c.id,
+    c.nombre
+FROM
+    clientes AS c
+    INNER JOIN pedidos AS p ON c.id = p.idCliente;
+
+-- ACT3: Productos y su Categoría
+Select
+    p.nombre as producto,
+    g.nombre as gama
 from
-    pedidos as p
-inner join clientes as c
-    on p.idCliente = c.id
+    productos as p
+    inner join gamas as g on g.ig = p.idGama;
+
+-- ACT4: Clientes con o sin pedidos
+Select
+    c.nombre,
+    count(p.*)
+from
+    clientes as p
+    left join pedidos on c.id = p.idCliente;
